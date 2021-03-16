f70f19d97a2ac9dcaffbe9858c2d40feeb474f54
Brovko Alexandra <brovko.sasha2002@gmail.com>
2021-03-16 11:01:47 +0200

    Add project files.

---
+++ HW10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random random = new Random();
+            for (int j = 0; j < arr.Length; j++)
+            {
+                arr[j] = random.Next(100);
+                Console.Write($"{arr[j]} ");
+            }
+
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            Console.WriteLine();
+            for (int j = 0; j < arr.Length; j++)
+            {
+                Console.Write(arr[j] + " ");
+            }
+            Console.Read();
+        }
+    }
+}
